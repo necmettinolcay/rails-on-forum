32258581131800f7b5f9f21119e8b6a2574f0454
necmettinolcay <olcay.ozer@bil.omu.edu.tr>
2015-02-24 22:45:00 +0200

    Kullanıcı hesap işlemleri

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,46 @@ class UsersController < ApplicationController
   		render :new
   	end
   end
-  
+
+  def show
+  	@user = User.find(params[:id])
+  	@data = []
+
+  	if params[:sayfa]
+  	  render layout: 'profile', locals: {page: params[:sayfa]}
+  	else
+  	  render layout: 'profile', locals: {page: 'konular' }
+  	end
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  	render layout: "profile"
+  end
+
+  def update
+  	@user = User.find(params[:id])
+
+  	update_params = user_params
+
+  	if update_params.has_key?(:password)
+  	  update_params.delete([:password, :password_confirmation])
+  	end
+
+    if @user.update(user_params)
+  	  flash[:notice] = "Profil bilgileriniz güncellendi."
+  	  redirect_to @user
+  	else
+  	  render :edit, layout: "profile"
+  	end
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+  	redirect_to '/'
+  end 
+
   private
   def user_params
   	params.require(:user).permit!
