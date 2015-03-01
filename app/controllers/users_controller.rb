1e80a0a6b00ebe2aba2bcf0c7d76e76f490c645c
necmettinolcay <olcay.ozer@bil.omu.edu.tr>
2015-03-01 19:12:03 +0200

    Profil sayfaları için bir kaç kısıtlama yapıldı

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :select_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :allowed?,    only: [:edit, :create, :destroy]
   def new
   	@user = User.new
   end
@@ -60,4 +60,11 @@ class UsersController < ApplicationController
   	@user = User.find_by_username(params[:id])
   end
 
+  def allowed?
+    user = select_user
+
+    unless current_user == user
+      redirect_to profile_path(user), alert: 'Bunu yapmaya yetkiniz yok!'
+    end
+  end
 end
